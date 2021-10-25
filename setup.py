835c878f869a7bd24083e5edc0913c841c2a74c7
magdalena@surowka.ch <magdalena@surowka.ch>
2021-10-25 18:53:59 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="graphly",
+    version="0.1",
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "statsmodels==0.10.2",
+        "matplotlib==3.2.2",
+        "sklearn>=0.0",
+        "sklearn-pandas==1.8.0",
+        "scikit-learn==0.22.2.post1",
+    ],
+)
